a5b7b30c8fd996ac32d8af3e8d7a200fa31637ae
agus Winarno <agus.winarno@hotmail.com>
2020-08-01 19:54:17 +0700

    initila xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by agus winarno on 8/1/20.
+//  Copyright © 2020 istana code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
